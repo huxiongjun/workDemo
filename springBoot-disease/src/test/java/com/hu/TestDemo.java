ef4a719d26295b203cb45d359f87da0f054b2c1b
huxiongjun <1678391922@qq.com>
2020-05-30 19:13:21 +0800

    测试

---
+++ springBoot-disease/src/test/java/com/hu/TestDemo.java
@@ -0,0 +1,22 @@
+package com.hu;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @author huxiongjun
+ * @version 1.0
+ * @date 2020/5/30 19:11
+ * @description 测试
+ */
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class TestDemo {
+    @Test
+    public void fun01(){
+        System.out.println("test");
+
+    }
+}
