4f9ad46332ef86c8cafb55803746df2bc8b27c99
huxiongjun <1678391922@qq.com>
2020-05-31 13:42:05 +0800

    111

--- springBoot-wangyi/src/test/java/com/hu/wangyi/TestDemo.java
+++ springBoot-wangyi/src/test/java/com/hu/wangyi/TestDemo.java
@@ -5,6 +5,8 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.UUID;
+
 /**
  * @author huxiongjun
  * @version 1.0
@@ -17,6 +19,10 @@ public class TestDemo {
     @Test
     public void fun01(){
         System.out.println("test");
+        UUID guid = UUID.randomUUID();
+        UUID panzhizhi = UUID.fromString("panzhizhi");
+        System.out.println(panzhizhi);
+        System.out.println(guid);
 
     }
 }
