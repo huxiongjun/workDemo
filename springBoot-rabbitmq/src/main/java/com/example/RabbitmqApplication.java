e6bf2c4de598b4c20ef553a647804ebdaca3f0ef
huxiongjun <1678391922@qq.com>
2020-06-03 21:19:13 +0800

    创建仓库

---
+++ springBoot-rabbitmq/src/main/java/com/example/RabbitmqApplication.java
@@ -0,0 +1,18 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author huxiongjun
+ * @version 1.0
+ * @date 2020/6/3 21:14
+ * @description 启动类
+ */
+@SpringBootApplication
+public class RabbitmqApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitmqApplication.class, args);
+    }
+
+}
